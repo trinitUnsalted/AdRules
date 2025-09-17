82cfc042c1d516415b5d0f6ad6e08dbca5e96b5f
YOUIMARK <91778001+trinitUnsalted@users.noreply.github.com>
2025-09-17 20:29:29 +0800

    Update readme.py

--- readme.py
+++ readme.py
@@ -75,7 +75,7 @@ class ReadMe(object):
         
         with open(self.filename, 'a') as f:
             f.write("# AdBlock DNS Filters\n")
-            f.write("去广告合并规则，每8个小时更新一次。  \n")
+            f.write("去广告合并规则，每4个小时更新一次。  \n")
             f.write("个人收藏了不少广告过滤规则，但是每次往新设备添加的时候很是头疼，于是写了这个项目，定时自动获取各规则源更新，生成合并规则库。\n")
             f.write("\n")
 
@@ -146,4 +146,4 @@ class ReadMe(object):
             f.write("\n")
             
             f.write("## Star History\n")
-            f.write("[![Star History Chart](https://api.star-history.com/svg?repos=trinitUnsalted/AdRules&type=Date)](https://star-history.com/#trinitUnsalted/AdRules&Date)\n")
+            f.write("[![Star History Chart](https://api.star-history.com/svg?repos=trinitUnsalted/AdRules&type=Date)](https://www.star-history.com/#trinitUnsalted/AdRules&Date)\n")
